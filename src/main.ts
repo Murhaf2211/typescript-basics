ce378e3202ff6a72d9c38f1a6df7d7196e684579
murhaf2211 <orfali.murhaf@web.de>
2024-09-19 11:32:44 +0200

    add array bonus task

--- src/main.ts
+++ src/main.ts
@@ -56,11 +56,37 @@ console.log(newNum);
 
 const words =["hello", "hi", "Hallochen", "Hey"];
 console.log(words);
+
+//Create an array of words. Use the '.filter' function to select only the words that are longer than 5 letters.
 const newWords = words.filter(word => word.length > 5 );
 console.log(newWords);
 
+//Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.
 const reduce = num.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 console.log(reduce);
 
+//Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.
 const some= num.some(num => num > 10);
 console.log(some);
+
+const list = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+
+//Sort these numbers in descending order.
+const sortDesNumbers = list.sort((a, b) => b - a);
+console.log(sortDesNumbers);
+
+//Square these numbers.
+const squareNumbers = list.map(num => num * num);
+console.log(squareNumbers);
+
+//Remove the lowest two numbers and the highest 4 numbers from the list.
+const removeNumbers = list.slice(4, -2);
+console.log(removeNumbers);
+
+//Remove all numbers that are divisible by 4.
+const removeNumberByDiv = list.filter(num => num %4 !==0);
+console.log(removeNumberByDiv);
+
+//Add up all the numbers.
+const sum = list.reduce((num, num1) => num +num1);
+console.log(sum);
